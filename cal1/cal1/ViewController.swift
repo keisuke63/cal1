288ed6f8f1aa842e3c125e2822e28337c397f6b8
momonnga <satoukeisuke@satou-no-MacBook-Air.local>
2015-05-04 23:00:58 +0900

    途中だけど

--- cal1/cal1/ViewController.swift
+++ cal1/cal1/ViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //計算結果を表示するラベルを宣言
+    var resultLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
-        
-        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //計算結果ラベルのフレームを設定。横幅は端末の画面サイズから左右の隙間20ポイントを引く
+        resultLabel.frame = CGRect(x: 10, y: 30, width: screenWidth-20, height: 170)
+        //計算結果ラベルの背景色を灰色にする
+        resultLabel.backgroundColor = UIColor.grayColor()
+     　　//計算結果ラベルのフォントと文字サイズを設定
+       　resultLabel.font = UIFont(name: "Arial", size: 50)
+        
+        
+        
+        
+        //計算結果ラベルをViewControllerクラスのviewに設置
+        self.view.addSubview(resultLabel)
+        
+    //画面の横幅のサイズを格納するメンバ変数
+        let screenWidth:Double = Double(UIScreen.mainScreen().bounds.size.width)
+        
+    
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
