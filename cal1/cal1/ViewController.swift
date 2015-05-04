22c9dfeecf716215ac28d150f512670bb973b2d7
momonnga <satoukeisuke@satou-no-MacBook-Air.local>
2015-05-04 14:32:56 +0900

    a

--- cal1/cal1/ViewController.swift
+++ cal1/cal1/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Labelを作成.計算結果
-        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,750,400))
+        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,9,10))
         
         // 背景をgray色にする.
         myLabel.backgroundColor = UIColor.brownColor()
@@ -38,7 +38,38 @@ class ViewController: UIViewController {
         myLabel.textAlignment = NSTextAlignment.Right
         
         // 配置する座標を設定する.
-        myLabel.layer.position = CGPoint(x: 0,y: 0)
+        myLabel.layer.position = CGPoint(x: 46,y: 46)
+        
+        // ViewにLabelを追加.
+        self.view.addSubview(myLabel)
+        
+        
+        // Labelを作成.1
+        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,93,94))
+        
+        // 背景をgray色にする.
+        myLabel.backgroundColor = UIColor.brownColor()
+        
+        // 枠を丸くする.
+        myLabel.layer.masksToBounds = true
+        
+        // コーナーの半径.
+        myLabel.layer.cornerRadius = 0
+        
+        // Labelに文字を代入.
+        myLabel.text = "0"
+        
+        // 文字の色を白にする.
+        myLabel.textColor = UIColor.whiteColor()
+        
+        // 文字の影の色をグレーにする.
+        myLabel.shadowColor = UIColor.grayColor()
+        
+        // Textを右寄せにする.
+        myLabel.textAlignment = NSTextAlignment.Right
+        
+        // 配置する座標を設定する.
+        myLabel.layer.position = CGPoint(x: 46,y: 141)
         
         // ViewにLabelを追加.
         self.view.addSubview(myLabel)
