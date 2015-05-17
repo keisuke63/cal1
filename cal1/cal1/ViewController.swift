63b67d5424b56e19873362e9dfda92929fc3e324
momonnga <satoukeisuke@satou-no-MacBook-Air.local>
2015-05-17 20:13:01 +0900

    2015.5.17-1

--- cal1/cal1/ViewController.swift
+++ cal1/cal1/ViewController.swift
@@ -13,25 +13,56 @@ class ViewController: UIViewController {
     //計算結果を表示するラベルを宣言
     var resultLabel = UILabel()
     
+    
+    //画面の横サイズを格納するメンバ変数
+    let screenWidth:Double = Double(UIScreen.mainScreen().bounds.size.width)
+    //画面の縦幅
+    let screenHidth:Double = Double(UIScreen.mainScreen().bounds.size.height)
+    //ボタン間の余白（縦）＆（横）
+    let buttonmargin = 10.0
+    //計算結果表示エリアの縦幅
+    var resultArea = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //画面全体の縦幅に応じて計算結果表示エリアの縦幅を決定
+        switch screenHidth {
+        case 480:
+            resultArea = 200.0
+        case 568:
+            resultArea = 250.0
+        case 667:
+            resultArea = 300.0
+        case 736:
+            resultArea = 350.0
+        default:
+            resultArea = 0.0
+        }
+        
+        
         //計算結果ラベルのフレームを設定。横幅は端末の画面サイズから左右の隙間20ポイントを引く
-        resultLabel.frame = CGRect(x: 10, y: 30, width: screenWidth-20, height: 170)
+        resultLabel.frame = CGRect(x: 10, y: 30, width: screenWidth-20, height: resultArea - 30)
         //計算結果ラベルの背景色を灰色にする
         resultLabel.backgroundColor = UIColor.grayColor()
-     　　//計算結果ラベルのフォントと文字サイズを設定
-       　resultLabel.font = UIFont(name: "Arial", size: 50)
-        
-        
-        
-        
-        //計算結果ラベルをViewControllerクラスのviewに設置
+//      　計算結果ラベルのフォントと文字サイズを設定
+        resultLabel.font = UIFont(name: "LetsgoDigital-Regular", size: 50)
+//        計算結果ラベルのアラインメントを右揃えに設定
+        resultLabel.textAlignment = NSTextAlignment.Right
+        //計算結果ラベルの表示行数を4桁に設定
+        resultLabel.numberOfLines = 4
+        //計算結果ラベルの初期値を"0"に設定
+        resultLabel.text = "0"
+        //計算結果ラベルをviewcontrollerクラスのviewに設置
         self.view.addSubview(resultLabel)
         
+        
+        
+        
+        
     //画面の横幅のサイズを格納するメンバ変数
-        let screenWidth:Double = Double(UIScreen.mainScreen().bounds.size.width)
+//        let screenWidth:Double = Double(UIScreen.mainScreen().bounds.size.width)
         
     
         
