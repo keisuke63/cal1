9a129ecd434e7d19513843441f8f38b8d4296f00
momonnga <satoukeisuke@satou-no-MacBook-Air.local>
2015-06-14 17:17:45 +0900

    ボタンタップ

--- cal1/cal1/ViewController.swift
+++ cal1/cal1/ViewController.swift
@@ -24,6 +24,13 @@ class ViewController: UIViewController {
     //計算結果表示エリアの縦幅
     var resultArea = 0.0
     
+    //入力数値1,入力数値2,計算結果,演算子
+    var number1:NSDecimalNumber = 0 // 入力数値を格納する変数1
+    var number2:NSDecimalNumber = 0 // 入力数値を格納する変数2
+    var result:NSDecimalNumber = 0  // 計算結果を格納する変数
+    var operatorId:String = ""      // 演算子を格納する変数
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -94,31 +101,89 @@ class ViewController: UIViewController {
         self.view.addSubview(button)
         
         //ボタンタップ時のアクション設定
-        button.addTarget(self, action: "buttonTapped", forControlEvents: TouchUpInside)
-        
-        //ボタンがタップされた時のメソッド
-                func buttonTapped(sender: UIButton)
-                {
-                    println("ボタンがタップされました")
-                }
+                button.addTarget(self, action: "buttonTapped", forControlEvents: UIControlEvents.TouchUpInside)
+                
                 
             }
+        }
     }
-        
-        
-    //画面の横幅のサイズを格納するメンバ変数
-//        let screenWidth:Double = Double(UIScreen.mainScreen().bounds.size.width)
-        
     
+        //ボタンがタップされた時のメソッド
+        func buttonTapped(sender: UIButton){
+            var tappedButtonTitle:String = sender.currentTitle!
+            println("\(tappedButtonTitle)ボタンがタップされました!")
+//         ボタンのタイトルで条件分岐
+            switch tappedButtonTitle {
+            case "0","1","2","3","4","5","6","7","8","9":   //数字ボタン
+            numberButtonTapped(tappedButtonTitle)
+            case "×","-","+","÷":                           //演算子ボタン
+            operatorButtonTapped(tappedButtonTitle)
+            case "=":                                       //等号ボタン
+            equalButtonTapped(tappedButtonTitle)
+            default:                                        //クリアボタン
+            clearButtonTapped(tappedButtonTitle)
+            
+        }
         
         
     }
 
+    func numberButtonTapped(tappedButtonTitle:String){
+        println("数字ボタンタップ:\(tappedButtonTitle)")
+        
+        // タップされた数字タイトルを計算できるようにDouble型に変換
+        var tappedButtonNum:NSDecimalNumber = NSDecimalNumber(string: tappedButtonTitle)
+        // 入力されていた値を10倍にして1桁大きくして、その変換した数値を加算
+        number1 = number1.decimalNumberByMultiplyingBy(NSDecimalNumber(string: "10")).decimalNumberByAdding(tappedButtonNum)
+        // 計算結果ラベルに表示
+        resultLabel.text = number1.stringValue
+}
+    func operatorButtonTapped(tappedButtonTitle:String){
+        println("演算子ボタンタップ:\(tappedButtonTitle)")
+        operatorId = tappedButtonTitle
+        number2 = number1
+        number1 = NSDecimalNumber(string: "0")
+    }
+    func equalButtonTapped(tappedButtonTitle:String){
+        println("等号ボタンタップ:\(tappedButtonTitle)")
+        switch operatorId {
+        case "+":
+            result = number2.decimalNumberByAdding(number1)
+        case "-":
+            result = number2.decimalNumberBySubtracting(number1)
+        case "×":
+            result = number2.decimalNumberByMultiplyingBy(number1)
+        case "÷":
+            if(number1.isEqualToNumber(0)){
+                number1 = 0
+                resultLabel.text = "無限大"
+                return
+            } else {
+                result = number2.decimalNumberByDividingBy(number1)
+            }
+        default:
+            println("その他")
+        }
+        number1 = result
+        resultLabel.text = String("\(result)")
+        
+    }
+    func clearButtonTapped(tappedButtonTitle:String){
+        println("クリアボタンタップ:\(tappedButtonTitle)")
+        number1 = NSDecimalNumber(string: "0")
+        number2 = NSDecimalNumber(string: "0")
+        result = NSDecimalNumber(string: "0")
+        operatorId = ""
+        resultLabel.text = "0"
+}
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+
+
 
